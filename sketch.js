52199dfa95de0c4f6a8bfeba420a41cb4de33658
Ruby Adkins <radkins22@students.stab.org>
2021-07-15 21:55:41 -0400

    have objects repel when mouse is clicked

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var font;
-var vehicles = [];
+var particles = [];
 
 function preload() {
   font = loadFont('AvenirNextLTPro-Demi.otf');
@@ -9,23 +9,32 @@ function setup() {
   createCanvas(600, 300);
   background(0);
 
-  var points = font.textToPoints('word', 100, 200, 192, {
+  var points = font.textToPoints('penis', 100, 200, 192, {
     sampleFactor: 0.25
   });
 
   for (var i = 0; i < points.length; i++) {
     var pt = points[i];
-    var vehicle = new Particle(pt.x, pt.y);
-    vehicles.push(vehicle);
+    particles.push(new Particle(pt.x, pt.y));
   }
 }
 
 function draw() {
   background(51);
-  for (var i = 0; i < vehicles.length; i++) {
-    var v = vehicles[i];
-    v.behaviors();
-    v.update();
-    v.show();
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].behaviors();
+    particles[i].update();
+    particles[i].show();
+  }
+}
+
+function mousePressed() {
+  var mouse = createVector(mouseX, mouseY);
+
+  for (let i = 0; i < particles.length; i++) {
+    var repel = particles[i].repel(mouse);
+    repel.mult(20);
+
+    particles[i].acc.add(repel);
   }
 }
