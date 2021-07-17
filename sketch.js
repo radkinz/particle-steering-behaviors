6cd883dcbf83c6a683d007a43af9e5b24cdb719d
Ruby Adkins <radkins22@students.stab.org>
2021-07-16 21:49:03 -0400

    particles starting position form an image

--- sketch.js
+++ sketch.js
@@ -1,26 +1,33 @@
 var font;
 var particles = [];
+var img;
 
 function preload() {
   font = loadFont('AvenirNextLTPro-Demi.otf');
+  img = loadImage('bjAlex.jpg')
 }
 
 function setup() {
-  createCanvas(600, 300);
-  background(0);
+  createCanvas(windowWidth, windowHeight);
 
-  var points = font.textToPoints('word', 50, 200, 192, {
-    sampleFactor: 0.25
-  });
-
-  for (var i = 0; i < points.length; i++) {
-    var pt = points[i];
-    particles.push(new Particle(pt.x, pt.y));
+  var points = [];
+  img.resize(width, height)
+  img.loadPixels();
+  for (let x = 0; x < img.width; x+=10) {
+    for (let y = 0; y < img.height; y+=10) {
+      let index = 4*(y*width+x);
+      let avg = (img.pixels[index] + img.pixels[index+1] + img.pixels[index+2])/3;
+      console.log(img.pixels[index], y, avg)
+      if (avg < 100) {
+        particles.push(new Particle(x, y));
+      }
+    }
   }
 }
 
 function draw() {
-  background(51);
+  background(0);
+  //image(img, 0, 0);
   for (var i = 0; i < particles.length; i++) {
     particles[i].behaviors();
     particles[i].update();
@@ -32,7 +39,7 @@ function mousePressed() {
   var mouse = createVector(width/2, height/2);
 
   for (let i = 0; i < particles.length; i++) {
-    var repel = particles[i].repel(mouse, random(300));
+    var repel = particles[i].repel(mouse, random(max(width/2, height/2)));
     repel.mult(20);
 
     particles[i].acc.add(repel);
